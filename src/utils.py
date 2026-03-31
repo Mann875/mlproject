e33c828919376f2a205f4dc99290e90211fe9b9b
Mann Miyani <mannmiyani007@gmail.com>
2026-03-31 13:52:12 -0400

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException 
 
 def save_object(file_path, obj):
@@ -16,11 +17,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys) # raising a custom exception if any error occurs during the saving of the object, and passing the error message and the system information to the custom exception class.
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         model_report = {} # initializing an empty dictionary to store the model report, which will contain the model name and the corresponding r2 score.
         for i in range(len(models)):
             model = list(models.values())[i] # getting the model from the models dictionary using the index, and storing it in the model variable.
+            
+            # Just as we listed all the model, List all the params
+            para = params[list(models.keys())[i]] # getting the parameters for the model from the params dictionary using the model name as the key, and storing it in the para variable.   
+            
+            gs = GridSearchCV(model, para, cv=3) # creating an instance of the GridSearchCV class, and passing the model, parameters and the number of cross validation folds as arguments to the class, so that it can perform hyperparameter tuning on the model using grid search cross validation.
+            gs.fit(X_train, y_train) # fitting the GridSearchCV object on the training data, so that it can find the best parameters for the model based on the training data.
+        
+            model.set_params(**gs.best_params_) # setting the best parameters found by the GridSearchCV object to the model, so that we can use the best parameters for training the model on the training data.    
             model.fit(X_train, y_train) # fitting the model on the training data, so that we can make predictions on the test data.
            
             y_train_pred = model.predict(X_train) # making predictions on the training data using the fitted model, and storing the predicted values in the y_train_pred variable.
